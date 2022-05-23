fa38731f0147a6f28b8e2a8943767e2a26673aeb
Conner Hogan <ConnerRHogan@gmail.com>
2022-05-23 02:57:20 -0600

    about css good enough

--- src/aboutMe.js
+++ src/aboutMe.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { Component } from 'react'
+
 import picture from './images/me.png'
 import js      from './images/js-logo.png'
 import python  from './images/python-logo.png'
@@ -21,7 +22,8 @@ export default class About extends Component {
         super(props)
         this.state = {
             me: picture,
-            skills:[js, python, jquery, react, node, flask, mongoDB,sql,css,html,socket,exp,taco,pie],
+            skills:[ js, python, jquery, react, node, flask, mongoDB, sql, css,
+                 html, socket, exp, taco, pie ],
         }
     }
 
@@ -30,15 +32,15 @@ export default class About extends Component {
             <div className="about">
 
             <span>
-            <img className="about img" src={this.state.me} alt="selfie" loading="lazy"/>
+            <img className="about-img" src={this.state.me} alt="selfie" loading="lazy"/>
 
-            <p className="pitch" >I am a detail oriented person. I bring a flexable working attitude.
+            <p className="pitch">"I am a detail oriented person. I bring a flexable working attitude.
                 working in various fields, I can easily fit into almost any team.
                 With almost two decades of work experience jumping into a new project
-                is not an issue.
+                is not an issue." <br/>
+                ~ Me
             </p>
             </span>
-
 
                 {/*
                 <div className="right">
@@ -47,31 +49,64 @@ export default class About extends Component {
                 </div>
                 */}
 
+            <h2>personality Traits</h2>
+            <span>
+                <ul className="traits">
+                    <li>creative approach to problem solving</li>
+                    <li>dependable</li>
+                    <li>Loves Tacos</li>
+                    <li>time management</li>
+                    <li>eager to learn new skills</li>
+                    <li>lots of experience</li>
+                    <li>flexable</li>
+                    <li>leader</li>
+                    <li>consistent</li>
+                    <li>Result driven</li>
+                    <li>Team player</li>
+                </ul>
+
+                <ul>
+                    <li>other traits on re review</li>
+                    <li>other traits on re review</li>
+                    <li>other traits on re review</li>
+                    <li>other traits on re review</li>
+                    <li>other traits on re review</li>
+
+                </ul>
+
+            </span>
 
 
-            <ul className="traits">
-                <li>creative approach to problem solving</li>
-                <li>dependable</li>
-                <li>Loves Tacos</li>
-                <li>time management</li>
-                <li>eager to learn new skills</li>
-                <li>lots of experience</li>
-                <li>flexable</li>
-                <li>leader</li>
-                <li>consistent</li>
-                <li>Result driven</li>
-                <li>Team player</li>
-            </ul>
+            <h3>Interests </h3>
+            <span>
+                <ul className="interests">
+                    <li>Bands</li>
+                    <li>....</li>
+                    <li>NIN</li>
+                    <li>Metallica</li>
+                    <li>tool</li>
+                    <li>more</li>
+                </ul>
 
+                <ul>
+                    <li>Movies</li>
+                    <li>....</li>
+                    <li>Movies</li>
+                    <li>Movies</li>
+                    <li>Movies</li>
+                    <li>Movies</li>
+                </ul>
+                <ul>
+                    <li>Other</li>
+                    <li>....</li>
+                    <li>My son</li>
+                    <li>my daughter</li>
+                    <li>3d pen</li>
+                    <li>building</li>
+                </ul>
 
+            </span>
 
-            <ul className="interests">
-                <li>Bands?</li>
-                <li>NIN</li>
-                <li>Metallica</li>
-                <li>tool</li>
-                <li>more</li>
-            </ul>
 
 
 
